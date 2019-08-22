fd37cfc9de0bbe1f45167fa7bac13c96b1ecba34
noellabo <noel.yoshiba@gmail.com>
2019-08-22 11:47:11 +0900

    Add favorite hashtags

--- app/javascript/mastodon/reducers/index.js
+++ app/javascript/mastodon/reducers/index.js
@@ -38,6 +38,7 @@ import missed_updates from './missed_updates';
 import announcements from './announcements';
 import markers from './markers';
 import picture_in_picture from './picture_in_picture';
+import favourite_tags from './favourite_tags';
 
 const reducers = {
   announcements,
@@ -79,6 +80,7 @@ const reducers = {
   missed_updates,
   markers,
   picture_in_picture,
+  favourite_tags,
 };
 
 export default combineReducers(reducers);
