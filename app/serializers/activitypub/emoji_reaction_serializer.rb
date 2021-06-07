933485dcecb0e89500bf9150c6d224f7d09b9854
noellabo <noel.yoshiba@gmail.com>
2021-06-08 07:19:09 +0900

    Change to interpret emoji reaction ⭐ as favorite

--- app/serializers/activitypub/emoji_reaction_serializer.rb
+++ app/serializers/activitypub/emoji_reaction_serializer.rb
@@ -3,7 +3,6 @@
 class ActivityPub::EmojiReactionSerializer < ActivityPub::Serializer
   attributes :id, :type, :actor, :content
   attribute :virtual_object, key: :object
-  attribute :misskey_reaction, key: :_misskey_reaction
 
   has_one :custom_emoji ,key: :tag, serializer: ActivityPub::EmojiSerializer, unless: -> { object.custom_emoji.nil? }
 
@@ -24,10 +23,6 @@ class ActivityPub::EmojiReactionSerializer < ActivityPub::Serializer
   end
 
   def content
-    object.custom_emoji.nil? ? object.name : ":#{object.name}:"
-  end
-
-  def misskey_reaction
     object.custom_emoji.nil? ? object.name : ":#{object.name}:"
   end
 end
