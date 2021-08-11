mirielnet/rdon

Add subscribe features

app/controllers/api/v1/keyword_subscribes_controller.rb
@@ -1,0 +1,48 @@
+# frozen_string_literal: true
+
+class Api::V1::KeywordSubscribeController < Api::BaseController
+  before_action -> { doorkeeper_authorize! :read, :'read:follows' }, only: [:index, :show]
+  before_action -> { doorkeeper_authorize! :write, :'write:follows' }, except: [:index, :show]
+  before_action :require_user!
+  before_action :set_keyword_subscribes, only: :index
+  before_action :set_keyword_subscribe, only: [:show, :update, :destroy]
+
+  respond_to :json
+
+  def index
+    render json: @keyword_subscribes, each_serializer: REST::KeywordSubscribeSerializer
+  end
+
+  def create
+    @keyword_subscribe = current_account.keyword_subscribes.create!(resource_params)
+    render json: @keyword_subscribe, serializer: REST::KeywordSubscribeSerializer
+  end
+
+  def show
+    render json: @keyword_subscribe, serializer: REST::KeywordSubscribeSerializer
+  end
+
+  def update
+    @keyword_subscribe.update!(resource_params)
+    render json: @keyword_subscribe, serializer: REST::KeywordSubscribeSerializer
+  end
+
+  def destroy
+    @keyword_subscribe.destroy!
+    render_empty
+  end
+
+  private
+
+  def set_keyword_subscribes
+    @keyword_subscribes = current_account.keyword_subscribes
+  end
+
+  def set_keyword_subscribe
+    @keyword_subscribe = current_account.keyword_subscribes.find(params[:id])
+  end
+
+  def resource_params
+    params.permit(:name, :keyword, :exclude_keyword, :ignorecase, :regexp, :ignore_block, :disabled, :exclude_home)
+  end
+end

app/workers/merge_worker.rb
@@ -5,7 +5,7 @@ class MergeWorker
 
   sidekiq_options queue: 'pull'
 
-  def perform(from_account_id, into_account_id)
-    FeedManager.instance.merge_into_timeline(Account.find(from_account_id), Account.find(into_account_id))
+  def perform(from_account_id, into_account_id, public_only = false)
+    FeedManager.instance.merge_into_timeline(Account.find(from_account_id), Account.find(into_account_id), public_only)
   end
 end

saba-chang/mastodon-for-fedibird

app/controllers/api/v1/keyword_subscribes_controller.rb
@@ -1,0 +1,49 @@
+
+# frozen_string_literal: true
+
+class Api::V1::KeywordSubscribesController < Api::BaseController
+  before_action -> { doorkeeper_authorize! :read, :'read:follows' }, only: [:index, :show]
+  before_action -> { doorkeeper_authorize! :write, :'write:follows' }, except: [:index, :show]
+  before_action :require_user!
+  before_action :set_keyword_subscribes, only: :index
+  before_action :set_keyword_subscribe, only: [:show, :update, :destroy]
+
+  respond_to :json
+
+  def index
+    render json: @keyword_subscribes, each_serializer: REST::KeywordSubscribesSerializer
+  end
+
+  def create
+    @keyword_subscribe = current_account.keyword_subscribes.create!(resource_params)
+    render json: @keyword_subscribe, serializer: REST::KeywordSubscribesSerializer
+  end
+
+  def show
+    render json: @keyword_subscribe, serializer: REST::KeywordSubscribesSerializer
+  end
+
+  def update
+    @keyword_subscribe.update!(resource_params)
+    render json: @keyword_subscribe, serializer: REST::KeywordSubscribesSerializer
+  end
+
+  def destroy
+    @keyword_subscribe.destroy!
+    render_empty
+  end
+
+  private
+
+  def set_keyword_subscribes
+    @keyword_subscribes = current_account.keyword_subscribes
+  end
+
+  def set_keyword_subscribe
+    @keyword_subscribe = current_account.keyword_subscribes.find(params[:id])
+  end
+
+  def resource_params
+    params.permit(:name, :keyword, :exclude_keyword, :ignorecase, :regexp, :ignore_block, :disabled, :list_id)
+  end
+end

app/workers/merge_worker.rb
@@ -3,8 +3,8 @@
 class MergeWorker
   include Sidekiq::Worker
 
-  def perform(from_account_id, into_account_id)
-    FeedManager.instance.merge_into_home(Account.find(from_account_id), Account.find(into_account_id))
+  def perform(from_account_id, into_account_id, public_only = false)
+    FeedManager.instance.merge_into_home(Account.find(from_account_id), Account.find(into_account_id), public_only)
   rescue ActiveRecord::RecordNotFound
     true
   ensure