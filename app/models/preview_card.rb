752bd558ed4aebca2a148cd92676516a79df19f1
noellabo <noel.yoshiba@gmail.com>
2021-11-25 10:17:40 +0900

    Add WebP support

--- app/models/preview_card.rb
+++ app/models/preview_card.rb
@@ -27,7 +27,7 @@
 #
 
 class PreviewCard < ApplicationRecord
-  IMAGE_MIME_TYPES = ['image/jpeg', 'image/png', 'image/gif'].freeze
+  IMAGE_MIME_TYPES = ['image/jpeg', 'image/png', 'image/gif', 'image/webp'].freeze
   LIMIT = 1.megabytes
 
   BLURHASH_OPTIONS = {
