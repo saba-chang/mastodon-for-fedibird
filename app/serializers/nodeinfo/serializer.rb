0124a17cf100ac12a8992678414c40d5d5e0ef30
noellabo <noel.yoshiba@gmail.com>
2022-06-28 14:06:25 +0900

    Change nodeinfo software name to fedibird

--- app/serializers/nodeinfo/serializer.rb
+++ app/serializers/nodeinfo/serializer.rb
@@ -10,7 +10,7 @@ class NodeInfo::Serializer < ActiveModel::Serializer
   end
 
   def software
-    { name: 'mastodon', version: Mastodon::Version.to_s }
+    { name: 'fedibird', version: Mastodon::Version.fedibird_verson }
   end
 
   def services
@@ -39,12 +39,10 @@ class NodeInfo::Serializer < ActiveModel::Serializer
 
   def metadata
     {
-      forks: [
-        {
-          name: 'fedibird',
-          version: Mastodon::Version.fedibird_verson,
-        },
-      ],
+      upstream: {
+        name: 'mastodon',
+        version: Mastodon::Version.to_s,
+      },
     }
   end
 
