a9a3fcda36f8c734c53734453da250495cf2e2a9
noellabo <noel.yoshiba@gmail.com>
2022-10-18 13:56:29 +0900

    Add remote domain validator for AccountDomainBlock

--- app/validators/remote_validator.rb
+++ app/validators/remote_validator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class DomainValidator < ActiveModel::EachValidator
+class RemoteValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return unless value.is_a?(String)
 
@@ -14,12 +14,6 @@ class DomainValidator < ActiveModel::EachValidator
 
     return if value.blank?
 
-    record.errors.add(attribute, I18n.t('domain_validator.invalid_domain')) unless compliant?(domain)
-  end
-
-  private
-
-  def compliant?(value)
-    value.match?(Twitter::TwitterText::Regex[:valid_domain])
+    record.errors.add(attribute, I18n.t('domain_validator.local_domain')) if TagManager.instance.local_domain?(domain) || TagManager.instance.web_domain?(domain)
   end
 end
